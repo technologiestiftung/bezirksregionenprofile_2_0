7935f3ab0924fc49af395cee12b1a2a799fa057f
rschulz <office@rgb3000.com>
2019-10-30 18:14:47 +0100

    basic meta tags for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -63,6 +63,19 @@ export default {
       menuOpen: false
     }
   },
+  head() {
+    return {
+      title: 'Home',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Profile zu Berliner Beziken'
+        }
+      ]
+    }
+  },
   computed: {
     ...mapState(['bzBzrPrData']),
     bzBzrPrDataSorted() {
