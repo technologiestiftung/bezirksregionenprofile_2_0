1bfae417ded4b2df6a03df9e67d1348fec964482
rschulz <office@rgb3000.com>
2019-11-04 15:26:07 +0100

    added aria label to map

--- components/bzr/contentElements/mapEl.vue
+++ components/bzr/contentElements/mapEl.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="pb-6">
-    <h5 v-if="dataEl.content !== ''">{{ dataEl.content }}</h5>
-    <bzr-map
-      v-if="dataEl.type === 'map-poi'"
-      class="mx-n5 mx-md-0"
-      :poi-data="dataEl.data"
-      :map-id="contentId"
-      map-type="content-map"
-    ></bzr-map>
-    <pr-map v-else class="mx-n5 mx-md-0" :data="dataEl.data" :map-id="contentId" map-type="content-map"></pr-map>
+    <h5 v-show="dataEl.content !== ''" :id="'label-' + contentId">{{ dataEl.content }}</h5>
+    <div role="application" :aria-labelledby="'label-' + contentId">
+      <bzr-map
+        v-if="dataEl.type === 'map-poi'"
+        class="mx-n5 mx-md-0"
+        :poi-data="dataEl.data"
+        :map-id="contentId"
+        map-type="content-map"
+      ></bzr-map>
+      <pr-map v-else class="mx-n5 mx-md-0" :data="dataEl.data" :map-id="contentId" map-type="content-map"></pr-map>
+    </div>
     <small v-if="dataEl.source != ''" class="content-source">{{ dataEl.source }}</small>
   </div>
 </template>
