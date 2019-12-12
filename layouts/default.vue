af6bad64ba4f355c17497e05007fc67fcae15040
rschulz <office@rgb3000.com>
2019-12-12 15:42:42 +0100

    moved berlin.de subnav into the nuxt app
    - pro: we can have nuxt link in subnav
    - con: dropdowns will not work any more because they get initialized with jquery on page load

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,58 @@
   <v-app>
     <v-content class="pa-0">
       <v-container fluid style="padding-left: 20px; padding-right:20px;">
+        <div class="row">
+          <div>
+            <div class="html5-header content-header" role="banner">
+              <div class="row">
+                <div class="span5">
+                  <div class="html5-section section-logo without-logo">
+                    <div class="html5-section text">
+                      <a
+                        href="/template/master/v4/index.php"
+                        title="Link führt zur Startseite von 'Institution Titel der Institution'"
+                      >
+                        <span class="title">Bezirksregionenprofile</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="span7"></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="span12">
+                <span class="mobile-blue-line hidden-desktop hidden-tablet"></span>
+                <div class="content-navi-wrapper navbar hidden-phone">
+                  <div class="html5-nav content-navi-top navbar-inner">
+                    <nav class="nav-collapse mainnav-collapse collapse" aria-label="Hauptnavigation">
+                      <!-- 3 template-path: tmpl_default/navi/navi_top_startseite.php -->
+                      <!-- template start: navi-top-start -->
+
+                      <ul class="nav level1 pl-0">
+                        <li class="has-submenu">
+                          <nuxt-link class="active" to="/">Kernindikatoren</nuxt-link>
+                        </li>
+                        <li class="has-submenu">
+                          <nuxt-link to="/about">Über das Projekt</nuxt-link>
+                        </li>
+                      </ul>
+
+                      <!-- /template end: navi-top-start -->
+                    </nav>
+
+                    <!-- 4 template-path: tmpl_default/header/beberlin.php -->
+                    <div class="beberlin">
+                      <img alt="Bild zeigt: Logo beBerlin" src="/berlinde/images/logo_beberlin_darkblue.png" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
         <nuxt />
       </v-container>
     </v-content>
